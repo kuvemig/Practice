1c8f5c0ad6e2457e01db3715575288789928820f
Donghan Lee <81956566+kuvemig@users.noreply.github.com>
2021-05-03 22:39:58 +0900

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,20 @@
+# 이메일전송하기
+import os
+os.getcwd()
+class Email:
+    sender =  ""
+
+    def send_mail(self, recv, subject, contents):
+        print("From:\t" + self.sender)
+        print("To\t" + recv)
+        print("Subject:" + subject)
+        print("Contents")
+        print(contents)
+        print("-"*20)
+
+e = Email()
+e.sender = "likelion_k@gmail.com"
+recv_list = ['1@gmail.com', '2@gmail.com', '3@gmail.com']
+
+for recv in recv_list:
+    e.send_mail(recv, "Welcome!", "This is contents")
